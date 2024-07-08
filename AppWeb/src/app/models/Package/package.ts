1162139c2cd7338649529981dc37df1f000bf50e
Yiztar <karol_254@outlook.com>
2024-07-08 09:05:10 -0600

    feat: implementacion de nuevas funcionalidades
    - Agregado de modelos de base de datos

---
+++ AppWeb/src/app/models/Package/package.ts
@@ -0,0 +1,10 @@
+//export class Package {
+    export interface Package{
+        
+        id: number,
+        N_Activities: string[],
+        N_Employs: string[],
+        Cost: string,
+
+    }
+//}
---
+++ AppWeb/src/app/models/Shopping_record/shopping-record.spec.ts
@@ -0,0 +1,7 @@
+import { ShoppingRecord } from './shopping-record';
+
+describe('ShoppingRecord', () => {
+  it('should create an instance', () => {
+    expect(new ShoppingRecord()).toBeTruthy();
+  });
+});
