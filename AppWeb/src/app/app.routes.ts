fcdfb7eac4b4e2527ea15eb464ef7f2be69cf722
Srismael <ismaelmtzballin@gmail.com>
2024-07-07 20:01:14 -0600

    feat: implementarion nuevas funcionalidades
    - Agregado de ruteo

--- AppWeb/src/app/app.routes.ts
+++ AppWeb/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponentComponent } from './components/landing-page-component/landing-page-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:LandingPageComponentComponent}
+];
