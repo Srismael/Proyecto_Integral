7abccffde88307505eec2ed11603e0a9e18a29f3
Talache <31emanuel21@gmail.com>
2024-07-08 17:33:32 -0600

    feat: SE AGREGARON NUEVAS FUNCIONALIDADES -se agrego el ruteo del componente empleados-

--- AppWeb/src/app/app.routes.ts
+++ AppWeb/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponentComponent } from './components/landing-page-component/landing-page-component.component';
+import { EmpleadosComponentComponent } from './components/empleados-component/empleados-component.component';
+
+
 
 export const routes: Routes = [
-    {path: '', component:LandingPageComponentComponent}
+    {path: '', component:LandingPageComponentComponent},
+    {path: 'Empleados', component:EmpleadosComponentComponent}
 ];
