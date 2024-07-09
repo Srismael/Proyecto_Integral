425ec50a3028dea9cd47def1b44863e1a37de316
Joovs <jovannaescogar9@gmail.com>
2024-07-09 17:49:12 -0600

    refactor: reorganizar la estructura del proyecto
    Se añadió una nueva ruta en en el archivo app.routes.ts, la ruta que se añadió es para el componente de Tarea
    
    Este commit fue Realizado por Charly y Jovanna

--- AppWeb/src/app/app.routes.ts
+++ AppWeb/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponentComponent } from './components/landing-page-component/landing-page-component.component';
 import { EmpleadosComponentComponent } from './components/empleados-component/empleados-component.component';
+import { TareaComponentComponent } from './components/tarea-component/tarea-component.component';
 
 
 
 export const routes: Routes = [
     {path: '', component:LandingPageComponentComponent},
-    {path: 'Empleados', component:EmpleadosComponentComponent}
+    {path: 'Empleados', component:EmpleadosComponentComponent},
+    {path: 'tarea', component:TareaComponentComponent}
 ];
