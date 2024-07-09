8c98a54e65a03685752196922c2e89f18e0d96a7
Nath Herrera <nathalieherrera0108@gmail.com>
2024-07-09 11:22:58 -0600

    refactor: Archivos del componente nav se movieron una carpeta mas arriba

--- AppWeb/src/app/app.routes.ts
+++ AppWeb/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponentComponent } from './components/landing-page-component/landing-page-component.component';
 import { EmpleadosComponentComponent } from './components/empleados-component/empleados-component.component';
+import { RegistroComponentComponent } from './components/registro-component/registro-component.component';
 
 
 
 export const routes: Routes = [
     {path: '', component:LandingPageComponentComponent},
-    {path: 'Empleados', component:EmpleadosComponentComponent}
+    {path: 'empleados', component:EmpleadosComponentComponent},
+    {path: 'registro', component:RegistroComponentComponent}
 ];
