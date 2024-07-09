a60357534afe97af6a3730622598d37f954eeb2d
Gefas <geraldzepedamtz@gmail.com>
2024-07-09 11:46:23 -0600

    feat: Nuevas caracteristícas o funcionalidades - Se cambio el contenido de Empleados a registro Realizado por Gerardo Zepeda Martínez

--- AppWeb/src/app/components/registro-component/registro-component.component.ts
+++ AppWeb/src/app/components/registro-component/registro-component.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';4
 
 @Component({
   selector: 'app-registro-component',
   standalone: true,
-  imports: [],
+  imports: [MatSlideToggleModule, MatButtonModule, MatInputModule, MatBottomSheetModule],
   templateUrl: './registro-component.component.html',
   styleUrl: './registro-component.component.css'
 })
