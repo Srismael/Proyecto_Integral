e3b21c07c12b4a073947c227a5dc70e0010e5f23
Gefas <geraldzepedamtz@gmail.com>
2024-07-08 21:02:59 -0600

    Feat: Agregado de nuevas caracteristícas -Se agrego el HTML, CSS y importaciones del componente Empleados Realizado por Gerardo Zepeda Martínez

--- AppWeb/src/app/components/empleados-component/empleados-component.component.ts
+++ AppWeb/src/app/components/empleados-component/empleados-component.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';4
+
 
 @Component({
   selector: 'app-empleados-component',
   standalone: true,
-  imports: [MatSlideToggleModule, MatButtonModule],
+  imports: [MatSlideToggleModule, MatButtonModule, MatInputModule, MatBottomSheetModule],
   templateUrl: './empleados-component.component.html',
   styleUrl: './empleados-component.component.css'
 })
