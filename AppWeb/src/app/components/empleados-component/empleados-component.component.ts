da23253a9f85e5f2a389fb029030618254367b4d
Gefas <geraldzepedamtz@gmail.com>
2024-07-08 18:21:19 -0600

    style: se realizo un cambio en Empledos Realizado por Gerardo Zepeda Martínez y Ismael Martínez Ballin

--- AppWeb/src/app/components/empleados-component/empleados-component.component.ts
+++ AppWeb/src/app/components/empleados-component/empleados-component.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-empleados-component',
   standalone: true,
-  imports: [],
+  imports: [MatSlideToggleModule, MatButtonModule],
   templateUrl: './empleados-component.component.html',
   styleUrl: './empleados-component.component.css'
 })
