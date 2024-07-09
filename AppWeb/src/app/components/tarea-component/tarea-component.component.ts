7935067cbdbd731097c4ef997a1cc84fb65af61c
Joovs <jovannaescogar9@gmail.com>
2024-07-09 17:37:52 -0600

    feat: agregar características de frontend en el componente de Tarea

--- AppWeb/src/app/components/tarea-component/tarea-component.component.ts
+++ AppWeb/src/app/components/tarea-component/tarea-component.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-tarea-component',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSelectModule,
+  ],
   templateUrl: './tarea-component.component.html',
   styleUrl: './tarea-component.component.css'
 })
