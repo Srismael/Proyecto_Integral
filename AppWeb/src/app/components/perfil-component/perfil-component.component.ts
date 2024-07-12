cdd1553937591f7839118504204ae972cbecb9de
Diego1887 <juandiegomartinezromo@gmail.com>
2024-07-12 16:20:16 -0600

    Feat: Agregado de vista perfil
    -Se añadio un nuevo html para la vista del perfil
    -Se añadió el css para el estilo de la vista
    -Se agregaron las importaciones necesarias para lo datos

--- AppWeb/src/app/components/perfil-component/perfil-component.component.ts
+++ AppWeb/src/app/components/perfil-component/perfil-component.component.ts
@@ -1,13 +1,20 @@
+// perfil-component.component.ts
 import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 
 @Component({
   selector: 'app-perfil-component',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatCardModule],
+            
   templateUrl: './perfil-component.component.html',
-  styleUrl: './perfil-component.component.css'
+  styleUrls: ['./perfil-component.component.css']
 })
 export class PerfilComponentComponent {
-
+  nombre = 'Juan Diego';
+  apellidos = 'Martínez Romo';
+  correo = 'juan.34mr@.com';
+  direccion = 'Emiliano Zapata 546-C';
 }
