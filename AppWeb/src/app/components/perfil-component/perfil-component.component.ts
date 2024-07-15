9705732d9616692f4929a035d787aaa64db7095d
Diego1887 <juandiegomartinezromo@gmail.com>
2024-07-15 11:45:45 -0600

    Feat: Agregado de nuevo código html y css
    -Se añandio nueva codificación para el html
    -Se añadio nuevo diseño css para el estilo de la vista

--- AppWeb/src/app/components/perfil-component/perfil-component.component.ts
+++ AppWeb/src/app/components/perfil-component/perfil-component.component.ts
@@ -1,20 +1,31 @@
-// perfil-component.component.ts
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
+import { MatListModule } from '@angular/material/list';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-perfil-component',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule],
-            
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    NgIf
+  ],
   templateUrl: './perfil-component.component.html',
   styleUrls: ['./perfil-component.component.css']
 })
 export class PerfilComponentComponent {
-  nombre = 'Juan Diego';
-  apellidos = 'Martínez Romo';
-  correo = 'juan.34mr@.com';
-  direccion = 'Emiliano Zapata 546-C';
 }
