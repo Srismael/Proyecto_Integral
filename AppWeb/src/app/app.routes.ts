f1b0bb604edc34de6ac50df5ce849c69c37be786
Leonardo <mateomax38@gmail.com>
2024-07-16 08:27:14 -0600

    feat: se implementaron nuevas funcionalidades y una funcion -Se agrego un estilado, 2 campos de input, un boton e importaciones para que la seccion de login este construido. Este commit fue Realizado por Karol y Leonardo

--- AppWeb/src/app/app.routes.ts
+++ AppWeb/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponentComponent } from './components/landing-page-component/landing-page-component.component';
 import { EmpleadosComponentComponent } from './components/empleados-component/empleados-component.component';
+import { LoginComponentComponent } from './components/login-component/login-component.component';
 
 
 
 export const routes: Routes = [
     {path: '', component:LandingPageComponentComponent},
-    {path: 'Empleados', component:EmpleadosComponentComponent}
+    {path: 'Empleados', component:EmpleadosComponentComponent},
+    {path: 'login', component:LoginComponentComponent}
 ];
--- AppWeb/src/app/components/login-component/login-component.component.ts
+++ AppWeb/src/app/components/login-component/login-component.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+
+import { Input, Component, Output, EventEmitter,  } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+
 
 @Component({
   selector: 'app-login-component',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatInputModule, MatButtonModule],
   templateUrl: './login-component.component.html',
   styleUrl: './login-component.component.css'
 })
+
+
 export class LoginComponentComponent {
 
+
 }
+
